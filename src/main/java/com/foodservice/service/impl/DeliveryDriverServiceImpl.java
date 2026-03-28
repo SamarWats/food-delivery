b56c63d1dfa8c2621467e1a1d11b6d1338f13bb3
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-03-28 15:23:29 +0530

    Resolved merge conflict

--- src/main/java/com/foodservice/service/impl/DeliveryDriverServiceImpl.java
+++ src/main/java/com/foodservice/service/impl/DeliveryDriverServiceImpl.java
@@ -1,9 +1,11 @@
 package com.foodservice.service.impl;
 
 import com.foodservice.entity.DeliveryDriver;
+import com.foodservice.entity.Order;
 import com.foodservice.entity.dto.DeliveryDriverResponseDTO;
 import com.foodservice.exception.DriverNotFoundException;
 import com.foodservice.repository.DeliveryDriverRepository;
+import com.foodservice.repository.OrderRepository;
 import com.foodservice.service.DeliveryDriverService;
 import com.foodservice.config.CustomMapper;
 
@@ -16,25 +18,49 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DeliveryDriverServiceImpl implements DeliveryDriverService {
 
-    private final DeliveryDriverRepository repo;
-    
-    public DeliveryDriverServiceImpl(DeliveryDriverRepository repo) {
-        this.repo = repo;
-    }
+    private final DeliveryDriverRepository deliveryDriverRepository=null;
+    private final OrderRepository orderRepository=null;
+
 
     @Override
     public DeliveryDriverResponseDTO getDriverById(Long id) {
-        DeliveryDriver driver = repo.findById(id).orElseThrow(() -> new DriverNotFoundException("Driver Not Found"));
+        DeliveryDriver driver = deliveryDriverRepository.findById(id)
+                .orElseThrow(() -> new DriverNotFoundException("Driver Not Found"));
+
         return CustomMapper.deliveryDriverToDTO(driver);
     }
 
     @Override
     public List<DeliveryDriverResponseDTO> getAllDrivers() {
-        return repo.findAll().stream().map(CustomMapper::deliveryDriverToDTO).toList();
+        return deliveryDriverRepository.findAll()
+                .stream()
+                .map(CustomMapper::deliveryDriverToDTO)
+                .toList();
     }
 
+//    @Override
+//    public List<DeliveryDriverResponseDTO> getDriverDeliveries(Long driverId) {
+//
+//        List<Order> orders = orderRepository.findOrdersByDriverId(driverId);
+//
+//        return orders.stream().map(order -> DriverDeliveryResponseDTO.builder()
+//                        .orderId(order.getOrderId())
+//                        .customerName(order.getCustomer().getCustomerName())
+//                        .restaurantName(order.getRestaurant().getRestaurantName())
+//                        .orderStatus(order.getOrderStatus())
+//                        .build()
+//                )
+//                .toList();
+//    }
+
     @Override
-    public List<DeliveryDriver> getDriversWithOrders() {
-        return repo.findDriversWithOrders();
+    public List<DeliveryDriverResponseDTO> getDriverDeliveries(Long driverId) {
+    	return null;
     }
+    
+//	@Override
+//	public List<DeliveryDriver> getDriversWithOrders() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 }
