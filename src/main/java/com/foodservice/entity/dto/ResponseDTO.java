f227f593a14a4677041bede8589c9d48c1d9572f
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-03-29 16:28:55 +0530

    fix(response): add constructor to ResponseDTO

--- src/main/java/com/foodservice/entity/dto/ResponseDTO.java
+++ src/main/java/com/foodservice/entity/dto/ResponseDTO.java
@@ -8,14 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseDTO {
-	private Integer statusCode;
-	private String message;
-	private Object data;
-	public ResponseDTO() {}
-	public ResponseDTO(int status, String message, Object data) {
-	    this.statusCode = status;
-	    this.message = message;
-	    this.data = data;
-	}
-	
+
+    private int status;
+    private String message;
+    private Object data;
 }
