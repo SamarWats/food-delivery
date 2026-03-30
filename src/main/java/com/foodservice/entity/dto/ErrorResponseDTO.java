5321099ebb42217f223992faa6f723ec5cb6c346
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-03-30 11:44:56 +0530

    new feat: delivery driver APIs and repository fixes

--- src/main/java/com/foodservice/entity/dto/ErrorResponseDTO.java
+++ src/main/java/com/foodservice/entity/dto/ErrorResponseDTO.java
@@ -9,10 +9,11 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class ErrorResponseDTO {
-    private LocalDateTime timestamp;
     private String apiPath;
     private HttpStatus errorCode;
     private String errorMessage;
+    private LocalDateTime timestamp;
 }
+
+
